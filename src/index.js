3dd72866e068d0d54f7d5d88be266f56ad27db71
kowalsking <kovvalsking@gmail.com>
2022-08-23 13:00:25 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = window.innerWidth;
+canvas.width = window.innerWidth
 canvas.height = window.innerHeight
